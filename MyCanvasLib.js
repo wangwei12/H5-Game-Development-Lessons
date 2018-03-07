b95fbcf645f9cb8a7c2e0ee60762e7002f2bc180
wangwei12 <2944982361@qq.com>
2018-03-07 17:16:54 +0800

    first commit

---
+++ MyCanvasLib.js
@@ -0,0 +1,10 @@
+(function () {
+    CanvasRenderingContext2D.prototype.fourpoint=function(x1,y1,x2,y2,x3,y3,x4,y4){
+        this.strokeStyle='red';
+        this.moveTo(x1,y1);
+        this.lineTo(x2,y2);
+        this.lineTo(x3,y3);
+        this.lineTo(x4,y4);
+        this.stroke();
+    }
+})();
